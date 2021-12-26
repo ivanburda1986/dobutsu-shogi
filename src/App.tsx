81b47020d9ddf89e7dc933e660d1fad5aa8e854b
ivanburda1986 <burda.ivan@gmail.com>
2021-12-26 01:36:07 +0100

    setting up firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { getCities } from "./FirebaseSetup";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 import "./App.css";
 
-function App() {
-  const getCitiesHandler = () => {
-    const cities = getCities();
-    console.log(cities);
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCBnawTeOf0cVa7m7aKFQoIqrXbJOorW2c",
+  authDomain: "dobutsushogi-43c6e.firebaseapp.com",
+  databaseURL: "https://dobutsushogi-43c6e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dobutsushogi-43c6e",
+  storageBucket: "dobutsushogi-43c6e.appspot.com",
+  messagingSenderId: "721291009374",
+  appId: "1:721291009374:web:16ce8eaf9286ec6a4683cf",
+  measurementId: "G-NHVXG2LCZJ",
+};
 
+//Init the firebase app
+initializeApp(firebaseConfig);
+
+//Init services
+const db = getFirestore();
+//Collection ref
+const colRef = collection(db, "books");
+//Get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    const books = snapshot.docs.map((book) => {
+      return { id: book.id, ...book.data() };
+    });
+    console.log(books);
+  })
+  .catch((error) => console.log(error.message));
+
+function App() {
   return (
     <div className="App">
       <h1>POC for real-time updates</h1>
@@ -15,7 +39,7 @@ function App() {
         <p>1</p>
       </div>
       <div className="controller">
-        <button onClick={() => getCitiesHandler()}>-</button>
+        <button>-</button>
         <button>+</button>
       </div>
     </div>
