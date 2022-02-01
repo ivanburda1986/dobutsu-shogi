2cb250a1d609da02bd726d875c5d9b915aab2cb2
ivanburda1986 <burda.ivan@gmail.com>
2022-02-01 23:58:16 +0100

    profile

--- src/Header/Avatar/Avatar.tsx
+++ src/Header/Avatar/Avatar.tsx
@@ -6,9 +6,5 @@ interface AvatarProps {
 }
 
 export const Avatar: React.FC<AvatarProps> = ({ name }) => {
-  return (
-    <button className={`${styles.avatar} d-flex flex-row justify-content-between align-items-center`}>
-      <div className={`${styles.avatarIco} d-flex flex-row justify-content-between align-items-center`} style={{ backgroundImage: `url("images/${name}.png")` }}></div>
-    </button>
-  );
+  return <div className={`${styles.avatarIco} d-flex flex-row justify-content-between align-items-center`} style={{ backgroundImage: `url("images/${name}.png")` }}></div>;
 };
--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useUpdateUserProfile } from "../api/firestore";
+import { Avatar } from "../Header/Avatar/Avatar";
+import styles from "./Profile.module.css";
 
 export const Profile: React.FC = () => {
   const updateUserProfile = useUpdateUserProfile;
-  React.useEffect(() => {
-    updateUserProfile({ displayName: "Ivanek Burda", photoURL: "https://www.nugo.cz/user/articles/images/jorksirsky-terier.jpg" });
-  }, []);
+
   return (
     <div className="container-lg my-3">
-      <h2>Profile</h2>
+      <h2>Avatar and username</h2>
+      <Container fluid className={`rounded py-1 ${styles.profileSectionContainer}`}>
+        <Row>
+          <Col className="d-flex flex-row justify-content-between align-items-center">
+            <div className="d-flex flex-row justify-content-between align-items-center">
+              <Avatar name="lion" />
+              <p className="fs-4 mx-2 align-middle my-auto">Ivanek</p>
+            </div>
+            <Button className={`${styles.profileSetupBtn} justify-content-center`}>Change</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
