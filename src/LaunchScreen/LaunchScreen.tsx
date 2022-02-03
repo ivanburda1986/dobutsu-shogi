085879c79ff92e5e1bf01f2a8837faa45c89d6fe
ivanburda1986 <burda.ivan@gmail.com>
2022-02-04 00:00:13 +0100

    Started the game class creation

--- src/LaunchScreen/LaunchScreen.tsx
+++ src/LaunchScreen/LaunchScreen.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 import styles from "./LaunchScreen.module.css";
 import sharedStyles from "../sharedStyles.module.css";
+import { Game } from "./newGameClass";
 
 export const LaunchScreen: React.FC = () => {
+  React.useEffect(() => {
+    const game = new Game({ name: "Ivanova hra", creator: "abcdef", type: "DOBUTSU" });
+    game.setOponentName("lukas");
+    console.log(game);
+  });
   return <div className="container-lg">Lanchscreen</div>;
 };
