0d1db900ce4bbb1c770ec368f6b471245ed0a28e
ivanburda1986 <burda.ivan@gmail.com>
2022-02-16 00:12:28 +0100

    players ui

---
+++ src/Session/PlayerInterface/PlayerInterface.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./PlayerInterface.module.css";
+
+export const PlayerInterface = () => {
+  return <div className={`${styles.PlayerInterface}`}>player interface</div>;
+};
