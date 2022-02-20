075f2e9cdffc44aef7868e5845cda9d9d7d3970a
iBurda <H50CHWA@s-mxs.net>
2022-02-20 16:56:57 +0100

    formatting with webstorm

--- src/App.tsx
+++ src/App.tsx
@@ -18,88 +18,88 @@ import { Session } from "./Session/Session";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export interface UserDataInterface {
-  email: string | null;
-  displayName: string | null;
-  photoURL: string | null;
+    email: string | null;
+    displayName: string | null;
+    photoURL: string | null;
 }
 
 export interface ProvidedContextInterface {
-  userLoggedIn: boolean;
-  loggedInUserEmail: string | null;
-  loggedInUserDisplayName: string | null;
-  loggedInUserUserId: string;
-  loggedInUserPhotoURL: string | null;
-  setUserData: ({ email, displayName, photoURL }: UserDataInterface) => void;
+    userLoggedIn: boolean;
+    loggedInUserEmail: string | null;
+    loggedInUserDisplayName: string | null;
+    loggedInUserUserId: string;
+    loggedInUserPhotoURL: string | null;
+    setUserData: ({email, displayName, photoURL}: UserDataInterface) => void;
 }
 
 export const App = () => {
-  const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
-  const [loggedInUserEmail, setLoggedInUserEmail] = useState<string | null>("");
-  const [loggedInUserDisplayName, setLoggedInUserDisplayName] = useState<string | null>("Username");
-  const [loggedInUserUserId, setLoggedInUserUserId] = useState<string>("");
-  const [loggedInUserPhotoURL, setLoggedInUserPhotoURL] = useState<string | null>("placeholder");
-  const navigate = useNavigate();
-  const location = useLocation();
+    const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
+    const [loggedInUserEmail, setLoggedInUserEmail] = useState<string | null>("");
+    const [loggedInUserDisplayName, setLoggedInUserDisplayName] = useState<string | null>("Username");
+    const [loggedInUserUserId, setLoggedInUserUserId] = useState<string>("");
+    const [loggedInUserPhotoURL, setLoggedInUserPhotoURL] = useState<string | null>("placeholder");
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  useEffect(() => {
-    if (userLoggedIn && location.pathname === "/login") {
-      console.log("App navigated from login");
-      navigate("../", { replace: false });
-      return;
-    }
-    if (userLoggedIn && location.pathname === "/register") {
-      console.log("App navigated from registration");
-      navigate("../", { replace: false });
-      return;
-    }
-    if (!userLoggedIn && location.pathname === "/") {
-      console.log("App navigated from login");
-      navigate("../login", { replace: false });
-      return;
-    }
-  }, [userLoggedIn, navigate]);
+    useEffect(() => {
+        if (userLoggedIn && location.pathname === "/login") {
+            console.log("App navigated from login");
+            navigate("../", {replace: false});
+            return;
+        }
+        if (userLoggedIn && location.pathname === "/register") {
+            console.log("App navigated from registration");
+            navigate("../", {replace: false});
+            return;
+        }
+        if (!userLoggedIn && location.pathname === "/") {
+            console.log("App navigated from login");
+            navigate("../login", {replace: false});
+            return;
+        }
+    }, [userLoggedIn, navigate]);
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setUserLoggedIn(true);
-      setLoggedInUserEmail(user.email);
-      setLoggedInUserDisplayName(user.displayName);
-      setLoggedInUserUserId(user.uid);
-      setLoggedInUserPhotoURL(user.photoURL);
-    } else {
-      setUserLoggedIn(false);
-    }
-  });
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setUserLoggedIn(true);
+            setLoggedInUserEmail(user.email);
+            setLoggedInUserDisplayName(user.displayName);
+            setLoggedInUserUserId(user.uid);
+            setLoggedInUserPhotoURL(user.photoURL);
+        } else {
+            setUserLoggedIn(false);
+        }
+    });
 
-  const setUserData = ({ email, displayName, photoURL }: UserDataInterface) => {
-    email && setLoggedInUserEmail(email);
-    displayName && setLoggedInUserDisplayName(displayName);
-    photoURL && setLoggedInUserPhotoURL(photoURL);
-  };
+    const setUserData = ({email, displayName, photoURL}: UserDataInterface) => {
+        email && setLoggedInUserEmail(email);
+        displayName && setLoggedInUserDisplayName(displayName);
+        photoURL && setLoggedInUserPhotoURL(photoURL);
+    };
 
-  const providedContext: ProvidedContextInterface = {
-    userLoggedIn,
-    loggedInUserEmail,
-    loggedInUserDisplayName,
-    loggedInUserUserId,
-    loggedInUserPhotoURL,
-    setUserData,
-  };
+    const providedContext: ProvidedContextInterface = {
+        userLoggedIn,
+        loggedInUserEmail,
+        loggedInUserDisplayName,
+        loggedInUserUserId,
+        loggedInUserPhotoURL,
+        setUserData,
+    };
 
-  return (
-    <div>
-      <AppContext.Provider value={providedContext}>
-        <Header />
-        <Routes>
-          <Route path="*" element={<LaunchScreen />} />
-          <Route path="/" element={<LaunchScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/creategame" element={<CreateGame />} />
-          <Route path="/session/:gameId" element={<Session />} />
-        </Routes>
-      </AppContext.Provider>
-    </div>
-  );
+    return (
+        <div>
+            <AppContext.Provider value={providedContext}>
+                <Header/>
+                <Routes>
+                    <Route path="*" element={<LaunchScreen/>}/>
+                    <Route path="/" element={<LaunchScreen/>}/>
+                    <Route path="/register" element={<RegisterScreen/>}/>
+                    <Route path="/login" element={<LoginScreen/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/creategame" element={<CreateGame/>}/>
+                    <Route path="/session/:gameId" element={<Session/>}/>
+                </Routes>
+            </AppContext.Provider>
+        </div>
+    );
 };
--- src/Session/Session.tsx
+++ src/Session/Session.tsx
@@ -14,24 +14,27 @@ import { PlayerInterface } from "./PlayerInterface/PlayerInterface";
 import { DocumentData } from "firebase/firestore";
 
 export const Session = () => {
-  const [amIOpponent, setAmIOpponent] = useState(false);
-  const { gameId } = useParams();
-  const appContext: ProvidedContextInterface = useContext(AppContext);
-  const [gameData, setGamedata] = useState<DocumentData | undefined>();
+    const [amIOpponent, setAmIOpponent] = useState(false);
+    const {gameId} = useParams();
+    const appContext: ProvidedContextInterface = useContext(AppContext);
+    const [gameData, setGamedata] = useState<DocumentData | undefined>();
 
-  useEffect(() => {
-    getSingleGameDetails({ gameId: gameId! }).then((doc) => {
-      let data = doc.data();
-      setGamedata(data);
-      if (evaluateBeingOpponent({ creatorId: data!.creatorId, loggedInUserUserId: appContext.loggedInUserUserId })) {
-        setAmIOpponent(true);
-      }
-    });
-  }, [appContext, gameId]);
+    useEffect(() => {
+        getSingleGameDetails({gameId: gameId!}).then((doc) => {
+            let data = doc.data();
+            setGamedata(data);
+            if (evaluateBeingOpponent({
+                creatorId: data!.creatorId,
+                loggedInUserUserId: appContext.loggedInUserUserId
+            })) {
+                setAmIOpponent(true);
+            }
+        });
+    }, [appContext, gameId]);
 
-  return (
-    <Container fluid className={styles.Session}>
-      <Board type="DOBUTSU" amIOpponent={amIOpponent} gameData={gameData} />
-    </Container>
-  );
+    return (
+        <Container fluid className={styles.Session}>
+            <Board type="DOBUTSU" amIOpponent={amIOpponent} gameData={gameData}/>
+        </Container>
+    );
 };
