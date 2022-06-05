e1081345a28aa5437e04e065082b80bd30cba646
ivanburda1986 <burda.ivan@gmail.com>
2022-06-06 00:46:26 +0200

    refresh the game interface realtime

--- src/Session/Session.tsx
+++ src/Session/Session.tsx
@@ -1,35 +1,56 @@
-import React, { useEffect, useState, useContext } from "react";
-import { Container } from "react-bootstrap";
-import { useParams } from "react-router";
-import { getSingleGameDetails } from "../api/firestore";
-import { AppContext } from "../context/AppContext";
+import React, {useEffect, useState, useContext, useRef} from "react";
+import {Container} from "react-bootstrap";
+import {useParams} from "react-router";
+import {gamesCollectionRef, getSingleGameDetails} from "../api/firestore";
+import {AppContext} from "../context/AppContext";
 
-import { ProvidedContextInterface } from "../App";
-import { Board } from "./Board/Board";
+import {ProvidedContextInterface} from "../App";
+import {Board} from "./Board/Board";
 
-import { evaluateBeingOpponent } from "./SessionService";
+import {evaluateBeingOpponent} from "./SessionService";
 
 import styles from "./Session.module.css";
-import { DocumentData } from "firebase/firestore";
+import {DocumentData, onSnapshot} from "firebase/firestore";
+import {ReturnedGameInterface} from "../LaunchScreen/WaitingGamesList/WaitingGamesList";
 
 export const Session = () => {
     const [amIOpponent, setAmIOpponent] = useState(false);
     const {gameId} = useParams();
     const appContext: ProvidedContextInterface = useContext(AppContext);
     const [gameData, setGameData] = useState<DocumentData | undefined>();
+    const isComponentMountedRef = useRef(true);
 
     useEffect(() => {
-        getSingleGameDetails({gameId: gameId!}).then((doc) => {
-            let data = doc.data();
-            setGameData(data);
-            if (evaluateBeingOpponent({
-                creatorId: data!.creatorId,
-                loggedInUserUserId: appContext.loggedInUserUserId
-            })) {
-                setAmIOpponent(true);
+        return () => {
+            isComponentMountedRef.current = false;
+        };
+    }, []);
+
+    // Make sure any game-related updates are reflected immediately
+    useEffect(() => {
+        onSnapshot(gamesCollectionRef, (snapshot) => {
+            if (isComponentMountedRef.current) {
+                let game: ReturnedGameInterface;
+                snapshot.docs.forEach((doc) => {
+                    game = doc.data() as ReturnedGameInterface;
+                    setGameData(game);
+                });
             }
         });
-    }, [appContext, gameId]);
+    }, []);
+
+    // Evaluate whether I am an opponent or creator and make sure my interface is turned
+    useEffect(() => {
+        if (!gameData) {
+            return;
+        }
+        if (evaluateBeingOpponent({
+            creatorId: gameData!.creatorId,
+            loggedInUserUserId: appContext.loggedInUserUserId
+        })) {
+            setAmIOpponent(true);
+        }
+    }, [appContext.loggedInUserUserId, gameData]);
 
     return (
         <Container fluid className={styles.Session}>
