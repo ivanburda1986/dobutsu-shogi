db302d9a33d44e967e719043431ad3fa10f4dd1b
ivanburda1986 <burda.ivan@gmail.com>
2022-06-12 16:55:58 +0200

    games

--- src/LaunchScreen/LaunchScreen.tsx
+++ src/LaunchScreen/LaunchScreen.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Container} from "react-bootstrap";
-import {WaitingGamesList} from "./WaitingGamesList/WaitingGamesList";
+import {ReturnedGameInterface, WaitingGamesList} from "./WaitingGamesList/WaitingGamesList";
 import {YourGamesInProgressList} from "./YourGamesInProgressList/YourGamesInProgress";
+import {collection, onSnapshot} from "firebase/firestore";
+import {db} from "../api/firestore";
 
 export const LaunchScreen: React.FC = () => {
+    const [games, setGames] = useState<ReturnedGameInterface[]>([]);
+
+    useEffect(() => {
+        const colRef = collection(db, "games");
+        onSnapshot(colRef, (snapshot) => {
+
+            const returnedGames = snapshot.docs.map((game) => {
+                return {...game.data()};
+            });
+            setGames(returnedGames as ReturnedGameInterface[]);
+        });
+    }, []);
+
     return (
         <Container>
-            <WaitingGamesList/>
-            <YourGamesInProgressList/>
+            <WaitingGamesList games={games.filter((game) => game.status === "WAITING")}/>
+            <YourGamesInProgressList games={games.filter((game) => game.status === "WAITING")}/>
         </Container>
     );
 };
