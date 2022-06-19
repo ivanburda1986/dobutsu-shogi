3a7c4c71bf7d05dca63384bc9a63420dc808bf6d
ivanburda1986 <burda.ivan@gmail.com>
2022-06-19 23:58:47 +0200

    styling recent move

--- src/Session/RecentMoves/RecentMoves.tsx
+++ src/Session/RecentMoves/RecentMoves.tsx
@@ -3,6 +3,7 @@ import styles from "./RecentMoves.module.css";
 import {MoveInterface} from "../../api/firestore";
 import {Avatar} from "../../Header/Avatar/Avatar";
 import {RiSwordFill} from 'react-icons/ri';
+import {GiOpenChest} from 'react-icons/gi';
 
 interface RecentMovesInterface {
     moves: MoveInterface[];
@@ -21,35 +22,38 @@ export const RecentMoves: FunctionComponent<RecentMovesInterface> = ({moves = []
 
 
     if (!lastMove) {
-        return (<div className={`${styles.RecentMoves}  d-flex justify-content-center align-items-center m`}>
+        return (<div className={`${styles.OpponentBg}  d-flex justify-content-center align-items-center m`}>
             <h6><strong>Latest move:</strong></h6>
         </div>);
     } else {
         return (
-            <div className={`${styles.RecentMoves}  d-flex justify-content-center align-items-center m`}>
+            <div className={`d-flex justify-content-center align-items-center m`}>
                 <h6 className="me-3"><strong>Latest move:</strong></h6>
-                {lastMove.isTakeOver && (<Avatar name={lastButOneMove!.type.toLowerCase()}
-                                                 square={true}/>)}
-                {lastMove.isTakeOver && lastButOneMove?.fromCoordinates}
-                {lastMove.isTakeOver &&
+                {
+                    lastMove.isTakeOver && <div className={styles.OpponentBg}>
+                        {(<Avatar name={lastButOneMove!.type.toLowerCase()}
+                                  square={true}/>)}
+                        {lastButOneMove?.fromCoordinates}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                            <path fillRule="evenodd"
+                                  d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                        </svg>
+                        {lastButOneMove?.targetCoordinates}
+                        {<RiSwordFill/>}
+                    </div>
+                }
+                <div className={styles.OpponentBg}>
+                    <Avatar name={lastMove.type.toLowerCase()}
+                            square={true}/>
+                    {lastMove.fromCoordinates}
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-arrow-right-short" viewBox="0 0 16 16">
                         <path fillRule="evenodd"
                               d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                    </svg>}
-                {lastMove.isTakeOver && lastButOneMove?.targetCoordinates}
-                {lastMove.isTakeOver && <RiSwordFill/>}
-                <Avatar name={lastMove.type.toLowerCase()}
-                        square={true}/>
-                {lastMove.fromCoordinates}
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                    <path fillRule="evenodd"
-                          d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                </svg>
-                {lastMove.targetCoordinates}
-
-
+                    </svg>
+                    {lastMove.targetCoordinates.length <= 2 ? lastMove.targetCoordinates : <GiOpenChest/>}
+                </div>
             </div>
         );
     }
