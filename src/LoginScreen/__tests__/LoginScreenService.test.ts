b2180a6f684a98a37d12363717b5326e0b69711f
ivanburda1986 <burda.ivan@gmail.com>
2022-08-04 00:18:51 +0200

    tests

---
+++ src/LoginScreen/__tests__/LoginScreenService.test.ts
@@ -0,0 +1,21 @@
+import {validatePasswordInputLength} from '../LoginScreenService';
+
+describe('loginScreenService', () => {
+    describe('validatePasswordInputLength', () => {
+        it('returns true when password has at least 1 character', () => {
+            const password = "12";
+
+            const isValid = validatePasswordInputLength(password);
+
+            expect(isValid).toBe(true);
+        });
+
+        it('returns false when password is not provided', () => {
+            const password = undefined;
+
+            const isValid = validatePasswordInputLength(password);
+
+            expect(isValid).toBe(false);
+        });
+    });
+});
