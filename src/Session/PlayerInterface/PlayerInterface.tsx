bad9dce3df2352962ada12a2fd4ca2f0bf31e5a9
ivanbururda1986 <burda.ivan@gmail.com>
2022-11-04 00:35:38 +0100

    interface

--- src/Session/PlayerInterface/PlayerInterface.tsx
+++ src/Session/PlayerInterface/PlayerInterface.tsx
@@ -8,28 +8,24 @@ import {v4 as uuidv4} from "uuid";
 import {DocumentData} from "firebase/firestore";
 import {StashRow} from "../Board/StashRow/StashRow";
 
-interface PlayerInterfaceInterface {
+interface PlayerInterfaceProps {
     amIOpponent: boolean;
     creatorInterface: boolean;
     gameData: DocumentData | undefined;
 }
 
-export const PlayerInterface: FC<PlayerInterfaceInterface> = ({
 
-                                                                  amIOpponent,
-                                                                  creatorInterface,
-                                                                  gameData
-                                                              }) => {
-    const appContext: AppContextInterface = useContext(AppContext);
+const getBorderStyle = (creatorInterface:boolean,gameData:DocumentData|undefined) => {
+    return  `${styles.PlayerInterface} ${isPlayersTurn({creatorInterface,gameData}) ? styles.PlayerInterfaceCurrentTurn : styles.PlayerInterfaceNotOnTurn} mx-3 my-1`
+}
+
+export const PlayerInterface: FC<PlayerInterfaceProps> = ({amIOpponent,creatorInterface,gameData}) => {
     const [rowNumbers, setRowNumbers] = useState<number[]>(getStashSize(creatorInterface).rowNumbers);
     const [columnLetters, setColumnLetters] = useState<string[]>(getStashSize(creatorInterface).columnLetters);
 
     return (
         <div
-            className={`${styles.PlayerInterface} ${isPlayersTurn({
-                creatorInterface,
-                gameData
-            }) ? styles.PlayerInterfaceCurrentTurn : styles.PlayerInterfaceNotOnTurn}  mx-3 my-1`}
+            className={getBorderStyle(creatorInterface,gameData)}
             style={{transform: `rotate(${creatorInterface === true ? 0 : 180}deg)`}}>
             <div
                 className={`${creatorInterface ? styles.CreatorHeader : styles.OpponentHeader} d-flex justify-content-between align-items-center rounded ps-1 pe-3 mb-1 p-1`}>
