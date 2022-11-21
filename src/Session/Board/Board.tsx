39dcb0ec11d6826d0b9cda7e348ffb01dcc6ca6e
ivanburda1986 <burda.ivan@gmail.com>
2022-11-22 00:58:32 +0100

    ref board

--- src/Session/Board/Board.tsx
+++ src/Session/Board/Board.tsx
@@ -1,8 +1,7 @@
-import { Dispatch, FC, SetStateAction, useContext, useEffect, useState } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Container } from "react-bootstrap";
-import { collection, doc, DocumentData, onSnapshot } from "firebase/firestore";
-import { db } from "../../api/firestore";
+import { DocumentData } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
 
 import { AppContextInterface } from "../../App";
@@ -12,8 +11,8 @@ import { PlayerInterface } from "../PlayerInterface/PlayerInterface";
 import { Stone, StoneInterface } from "./Stones/Stone";
 import { getBackground } from "../../images/imageRelatedService";
 import { isThisPlayerOpponent } from "../SessionService";
+import { getColumnLetters, getRowNumbers, listenToStonePositionChange } from "./BoardService";
 import styles from "./Board.module.css";
-import { ReturnedGameInterface } from "../../LaunchScreen/Game/Game";
 
 interface BoardInterface {
     gameData: DocumentData | undefined;
@@ -32,75 +31,28 @@ export const Board: FC<BoardInterface> = ({gameData}) => {
     const amIOpponent = isThisPlayerOpponent(gameData?.creatorId, loggedInUserUserId);
 
     const [stones, setStones] = useState<StoneInterface[]>([]);
-    const [rowNumbers, setRowNumbers] = useState<number[]>([1, 2, 3, 4]);
-    const [columnLetters, setColumnLetters] = useState<string[]>(["A", "B", "C"]);
     const [draggedStone, setDraggedStone] = useState<StoneInterface | undefined>();
     const [lyingStone, setLyingStone] = useState<StoneInterface | undefined>();
     const [canTakeStone, setCanTakeStone] = useState<boolean>(false);
-    const [,setWinner] = useState<string>();
-    const [,setVictoryType] = useState<VictoryType>();
-
-
-    interface StonePositionChangeListenerInterface {
-        updateState: Dispatch<SetStateAction<StoneInterface[]>>;
-        gameId?: string;
-    }
-
-    const listenToStonePositionChange = ({updateState, gameId}:StonePositionChangeListenerInterface) => {
-        onSnapshot(collection(db, `games/${gameId}/stones`), (snapshot) => {
-            let returnedStones: StoneInterface[] = [];
-            snapshot.docs.forEach((doc) => {
-                returnedStones.push({...doc.data()} as StoneInterface);
-            });
-            setTimeout(() => updateState(returnedStones), 100);
-        });
-    }
-
-    const listenToGameStateChange = () => {
-        const docRef = doc(db, "games", gameId!);
-        onSnapshot(docRef, (doc) => {
-            setWinner(doc.data()?.winner);
-            setVictoryType(doc.data()?.victoryType);
-        });
-    }
 
     useEffect(() => {
         //Listening to change of stone positions
         listenToStonePositionChange({updateState:setStones, gameId:gameId})
-
-        //Listening to change of the game state (victory/defeat)
-        const docRef = doc(db, "games", gameId!);
-        onSnapshot(docRef, (doc) => {
-            setWinner(doc.data()?.winner);
-            setVictoryType(doc.data()?.victoryType);
-        });
-
     }, [gameId]);
 
-    useEffect(() => {
-        if (amIOpponent) {
-            setRowNumbers(rowNumbers.reverse());
-            setColumnLetters(columnLetters.reverse());
-        }
-    }, [amIOpponent, columnLetters, rowNumbers]);
-
     return (
-        <Container id="board"
-                   className={`mb-4 ${styles.Board}  ${amIOpponent ? styles.OpponentLayout : styles.CreatorLayout}`}>
-
+        <Container id="board" className={`mb-4 ${styles.Board}  ${amIOpponent ? styles.OpponentLayout : styles.CreatorLayout}`}>
             <div
                 className={`${styles.Interface1}`}
                 style={{transform: `rotate(${amIOpponent ? 180 : 0}deg)`}}>
-                <PlayerInterface amIOpponent={amIOpponent} creatorInterface={false} gameData={gameData}
-
-                />
+                <PlayerInterface amIOpponent={amIOpponent} creatorInterface={false} gameData={gameData}/>
             </div>
             <div
                 className={`my-3 my-md-0 ${styles.Board}`}>
                 <div style={{backgroundImage: `url(${amIOpponent ? getBackground({rotated:true}) : getBackground({rotated:false})})`}}
                      className={`${styles.BoardBg}`}>
-                    {rowNumbers.map((item) => (
-                        <BoardRow key={uuidv4()} rowNumber={item} columnLetters={columnLetters}
+                    {getRowNumbers(amIOpponent).map((item) => (
+                        <BoardRow key={uuidv4()} rowNumber={item} columnLetters={getColumnLetters(amIOpponent)}
                                   amIOpponent={amIOpponent}
                                   gameData={gameData} stones={stones}
                         />
@@ -118,8 +70,8 @@ export const Board: FC<BoardInterface> = ({gameData}) => {
                             invisible={stone.invisible}
                             positionLetter={stone.positionLetter}
                             positionNumber={stone.positionNumber}
-                            rowNumbers={rowNumbers}
-                            columnLetters={columnLetters}
+                            rowNumbers={getRowNumbers(amIOpponent)}
+                            columnLetters={getColumnLetters(amIOpponent)}
                             draggedStone={draggedStone}
                             lyingStone={lyingStone}
                             setDraggedStone={setDraggedStone}
