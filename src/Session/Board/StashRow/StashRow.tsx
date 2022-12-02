8b8608849f242b437e5e2f28c8f880f3df8db887
ivanburda1986 <burda.ivan@gmail.com>
2022-12-02 12:31:31 +0100

    stashfield refactore

--- src/Session/Board/StashRow/StashRow.tsx
+++ src/Session/Board/StashRow/StashRow.tsx
@@ -1,20 +1,24 @@
-import {FC} from "react";
-import {v4 as uuidv4} from "uuid";
+import { FC } from "react";
+import { v4 as uuidv4 } from "uuid";
 import styles from "./StashRow.module.css";
-import {StashField} from "../StashField/StashField";
-import {getStashColumnLetters} from "../../PlayerInterface/PlayerInterfaceService";
+import { StashField } from "../StashField/StashField";
+import { getStashColumnLetters } from "../../PlayerInterface/PlayerInterfaceService";
 
 interface StashRowInterface {
-    creatorInterface: boolean;
-    amIOpponent: boolean;
+  creatorInterface: boolean;
+  amIOpponent: boolean;
 }
 
-export const StashRow: FC<StashRowInterface> = ({ creatorInterface, amIOpponent}) => {
-    const columnLetters = getStashColumnLetters(creatorInterface)
-    return <div className={`${styles.StashRow}`}>
-        {columnLetters.map((letter) =>
-            <StashField key={uuidv4()}
-                        columnLetter={letter}
-                        amIOpponent={amIOpponent}/>
-        )}</div>;
+export const StashRow: FC<StashRowInterface> = ({
+  creatorInterface,
+  amIOpponent,
+}) => {
+  const columnLetters = getStashColumnLetters(creatorInterface);
+  return (
+    <div className={`${styles.StashRow}`}>
+      {columnLetters.map((letter) => (
+        <StashField key={uuidv4()} columnLetter={letter} />
+      ))}
+    </div>
+  );
 };
