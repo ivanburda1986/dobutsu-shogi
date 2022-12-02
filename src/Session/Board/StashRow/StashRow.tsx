830fe01173c5618a5c77551793a5a5a8082eccc0
ivanburda1986 <burda.ivan@gmail.com>
2022-12-02 12:34:12 +0100

    stashrow refactore

--- src/Session/Board/StashRow/StashRow.tsx
+++ src/Session/Board/StashRow/StashRow.tsx
@@ -1,19 +1,15 @@
 import { FC } from "react";
 import { v4 as uuidv4 } from "uuid";
-import styles from "./StashRow.module.css";
 import { StashField } from "../StashField/StashField";
 import { getStashColumnLetters } from "../../PlayerInterface/PlayerInterfaceService";
+import styles from "./StashRow.module.css";
 
 interface StashRowInterface {
-  creatorInterface: boolean;
-  amIOpponent: boolean;
+  isCreatorInterface: boolean;
 }
 
-export const StashRow: FC<StashRowInterface> = ({
-  creatorInterface,
-  amIOpponent,
-}) => {
-  const columnLetters = getStashColumnLetters(creatorInterface);
+export const StashRow: FC<StashRowInterface> = ({ isCreatorInterface }) => {
+  const columnLetters = getStashColumnLetters(isCreatorInterface);
   return (
     <div className={`${styles.StashRow}`}>
       {columnLetters.map((letter) => (
