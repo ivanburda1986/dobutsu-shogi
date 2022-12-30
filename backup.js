ae4c6df17516b5696d8479f73e7d6ded1a8e5556
ivanburda1986 <burda.ivan@gmail.com>
2022-12-30 20:46:56 +0100

    fix

---
+++ backup.js
@@ -0,0 +1,10 @@
+// package.json
+
+// "jest": {
+//     "transform": {
+//         ".(ts|tsx)": "<rootDir>/node_modules/ts-jest"
+//     },
+//     "transformIgnorePatterns": [
+//         "node_modules/(?!variables/.*)"
+//     ]
+// }
